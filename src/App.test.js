f063340d0031765b23b74d97efee0c6b0a3ca33d
Tom Mills <t.mills124@gmail.com>
2022-09-03 13:38:38 +0100

    project set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders weather app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Weather App/i);
+  expect(titleElement).toBeInTheDocument();
 });
