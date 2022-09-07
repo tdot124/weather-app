8c43eab1a1bc69ad3064b03e6d83e1a61256238a
Tom Mills <t.mills124@gmail.com>
2022-09-07 20:35:02 +0100

    added ForecastSummary component to render summary information for each day

---
+++ src/components/ForecastSummary.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ForecastSummary(props) {
+  const {date, description, icon, temperature} = props;
+  return (
+    <div className="forecast-summary">
+      <div className="forecast-summary__date">
+        {date}
+      </div>
+      <div className="forecast-summary__icon">
+        {icon}
+      </div>
+      <div className="forecast-summary__temperature">
+        {temperature.max}
+      </div>
+      <div className="forecast-summary__description">
+        {description}
+      </div>
+    </div>
+  );
+};
+
+export default ForecastSummary;
+
+ForecastSummary.propTypes = {
+  date: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.number.isRequired,
+  temperature: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }).isRequired,
+};
