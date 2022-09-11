a9ce732a92e1856100d0e4e7c681fb8e0f60bf13
Tom Mills <t.mills124@gmail.com>
2022-09-11 17:55:04 +0100

    added prop/value tests for ForecastSummary to ensure correct properties are rendering

--- src/components/ForecastSummary.jsx
+++ src/components/ForecastSummary.jsx
@@ -6,8 +6,12 @@ function ForecastSummary(props) {
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
-      <div className="forecast-summary__icon">{icon}</div>
-      <div className="forecast-summary__temperature">{temperature.max}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        {icon}
+      </div>
+      <div className="forecast-summary__temperature">
+        {temperature.max}&deg;C
+      </div>
       <div className="forecast-summary__description">{description}</div>
     </div>
   );
--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -25,4 +25,32 @@ describe("ForecastSummary", () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it("renders correct values for props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(getByText("1111111")).toHaveAttribute(
+      "class",
+      "forecast-summary__date"
+    );
+    expect(getByText("Stub description")).toHaveAttribute(
+      "class",
+      "forecast-summary__description"
+    );
+    expect(getByTestId("forecast-icon")).toHaveAttribute(
+      "class",
+      "forecast-summary__icon"
+    );
+    expect(getByText("22°C")).toHaveAttribute(
+      "class",
+      "forecast-summary__temperature"
+    );
+  });
 });
