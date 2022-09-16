ba5d466d869202e24b8f01919ba803f51eb31ae7
Tom Mills <t.mills124@gmail.com>
2022-09-16 07:24:15 +0100

    fixed failing tests due to previous changes

---
+++ src/tests/components/App.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../../components/App";
+
+describe("App", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -3,34 +3,35 @@ import { render } from "@testing-library/react";
 import ForecastSummaries from "../../components/ForecastSummaries";
 
 describe("ForecastSummaries", () => {
-  const validProps = [
-    {
-      date: 1111111,
-      description: "Stub description1",
-      icon: "511",
-      temperature: {
-        min: 11,
-        max: 22,
+  const validProps = {
+    forecasts: [
+      {
+        date: 1111111,
+        description: "Stub description1",
+        icon: 511,
+        temperature: {
+          min: 11,
+          max: 22,
+        },
       },
-    },
-    {
-      date: 2222222,
-      description: "Stub description2",
-      icon: "602",
-      temperature: {
-        min: 22,
-        max: 33,
+      {
+        date: 2222222,
+        description: "Stub description2",
+        icon: 602,
+        temperature: {
+          min: 22,
+          max: 33,
+        },
       },
-    },
-  ];
-
-  const onForecastSelect = () => {};
+    ],
+    onForecastSelect: () => {},
+  };
 
   it("renders correctly", () => {
     const { asFragment } = render(
       <ForecastSummaries
-        forecasts={validProps}
-        onForecastSelect={onForecastSelect}
+        forecasts={validProps.forecasts}
+        onForecastSelect={validProps.onForecastSelect}
       />
     );
     expect(asFragment()).toMatchSnapshot();
@@ -39,8 +40,8 @@ describe("ForecastSummaries", () => {
   it("renders the correct amount of ForecastSummary elements", () => {
     const { getAllByTestId } = render(
       <ForecastSummaries
-        forecasts={validProps}
-        onForecastSelect={onForecastSelect}
+        forecasts={validProps.forecasts}
+        onForecastSelect={validProps.onForecastSelect}
       />
     );
     expect(getAllByTestId("forecast-summary")).toHaveLength(2);
